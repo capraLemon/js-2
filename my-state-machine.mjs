311baffa3a0a5fd36a7aaf5a3b8a0afff2ac8b9b
unknown <ultrakeel@gmail.com>
2019-01-06 14:38:57 +0300

    corrections

--- my-state-machine.mjs
+++ my-state-machine.mjs
@@ -1,106 +1,29 @@
 class Machine {
 
-    constructor(parameters) {
-        this.id = parameters.id;
-        this.initialState = parameters.initialState;
-        this.context = parameters.context;
-        this.states = parameters.states;
-        this.actions = parameters.actions;
-        this.futureTransitions = [];
-        this.transitionFailed = false;       
+    constructor({id, initialState, context, states, actions}) {
+        Object.assign(this, {id, initialState, context, states, actions});
+        this.currentState = initialState;
     }
 
-    transition(transaction, body, retry=true) {
-        let stateActions;
-        if (retry && this.transitionFailed) {
-            this.transitionFailed = false;
-            resolveTransition.call(this, this.futureTransitions[0][0], this.futureTransitions[0][1]);
-        } else if (this.transitionFailed) {
-            this.transitionFailed = false;
-            this.futureTransitions = [];
-            resolveTransition.call(this, transaction, body);
-        } else {
-            this.futureTransitions.push([transaction, body]);
-            if (this.futureTransitions.length !== 1) {
-                return;
-            }
-            resolveTransition.call(this, transaction, body);
-        }
-
-        function resolveTransition(transaction, body) {
-            stateActions = this.states[this.initialState];
+    transition(transaction, body) {
+        stackOfMachines.push(this);
+        let stateActions = this.states[this.currentState];
+        try {
             checkInitialErrors.call(this, transaction);
-            return changeState.call(this, transaction, body);
-        }
-
-        function changeState(transaction, body) {
-            (async () => {
-                try {
-                    await doAllActions.call(this, transaction, body);
-
-                    console.log('finished transition in machine with');
-                    console.log(`id: ${this.id}, state: ${this.initialState}, context.compl: ${this.context.completed}`);
-                    console.log();
-
-                    this.futureTransitions.shift();
-                    if (this.futureTransitions.length) {
-                        resolveTransition.call(this, this.futureTransitions[0][0], this.futureTransitions[0][1]);
-                    } else {
-                        return;
-                    }
-                } catch(err) {
-                    console.log(err);
-                }
-            })();
-        }
-
-        async function doAllActions(transaction, body) {
-            doAction.call(this, 'onExit');
-            await doTransaction.call(this, transaction, body);
-            doAction.call(this, 'onEntry');
-        }
-
-        function doTransaction(transaction, body) {
-            useState.currentMachine = this;
-            useContext.currentMachine = this;
-            return new Promise( (resolve) => {
-                if (stateActions.on[transaction].service) {
-                    (async () => {
-                        try {
-                            await stateActions.on[transaction].service(body);
-                            stateActions = this.states[this.initialState];
-                            resolve();
-                        } catch(err) {
-                            this.transitionFailed = true;
-                            console.log(`something went wrong in service action for ${transaction} transaction`);
-                            console.log(err);
-                        }
-                    })();
-                }
-            })
-        }
-
-        function doAction(onEntryOrExit) {
-            useState.currentMachine = this;
-            useContext.currentMachine = this;
-            if (!stateActions[onEntryOrExit]) {
-                return;
+            if (stateActions.on[transaction].service) {
+                stateActions.on[transaction].service(body);
             }
-            if (typeof(stateActions[onEntryOrExit]) === "function") {
-                stateActions[onEntryOrExit]();
-            } else if (this.actions[stateActions[onEntryOrExit]]) {
-                this.actions[stateActions[onEntryOrExit]]();
-            } else {
-                throw new Error(`there is no such ${onEntryOrExit} action in "actions" field`);
-            }
+            stackOfMachines.pop();
+        } catch(err) {
+            console.log(err);
         }
 
         function checkInitialErrors(transaction) {
-            if (!this.states[this.initialState]) {
-                throw new Error(`there is no such initial state ${this.initialState}`);
+            if (!this.states[this.currentState]) {
+                throw new Error(`there is no such state ${this.currentState}`);
             }
             if (!stateActions.on) {
-                throw new Error(`there is no "on" action in state ${this.initialState}`);
+                throw new Error(`there is no "on" action in state ${this.currentState}`);
             }
             if (!stateActions.on.hasOwnProperty(transaction)) {
                 throw new Error(`there is no transaction ${transaction} in "on" field`);
@@ -109,21 +32,44 @@ class Machine {
     }
 }
 
+let stackOfMachines = [];
+
+function doAction(onEntryOrExit) {
+    let stateActions = this.states[this.currentState];
+    if (!stateActions[onEntryOrExit]) {
+        return;
+    }
+    if (typeof(stateActions[onEntryOrExit]) === "function") {
+        stateActions[onEntryOrExit]();
+    } else if (this.actions[stateActions[onEntryOrExit]]) {
+        this.actions[stateActions[onEntryOrExit]]();
+    } else {
+        throw new Error(`there is no such ${onEntryOrExit} action in "actions" field`);
+    }
+}
+
 function useState() {
-    const currentMachine = useState.currentMachine;
-    return [currentMachine.initialState, 
+    const currentMachine = stackOfMachines[stackOfMachines.length - 1];
+    return [currentMachine.currentState,
         (newState) => {
+            stackOfMachines.push(currentMachine);
             if (!currentMachine.states[newState]) {
                 throw new Error(`can not set new state, ${newState} doesn't exist`);
             }
-            currentMachine.initialState = newState;
+            doAction.call(currentMachine, 'onExit');
+            currentMachine.currentState = newState;
+            doAction.call(currentMachine, 'onEntry');
+            stackOfMachines.pop();
         }
     ];
 }
 
 function useContext() {
-    const currentMachine = useContext.currentMachine;
-    return [currentMachine.context, (newContext) => {Object.assign(currentMachine.context, newContext)}];
+    const currentMachine = stackOfMachines[stackOfMachines.length - 1];
+    return [currentMachine.context, 
+        (newContext) => {
+            Object.assign(currentMachine.context, newContext);
+        }];
 }
 
 function machine(parameters) {
--- vacancyMachine.mjs
+++ vacancyMachine.mjs
@@ -1,126 +1,73 @@
 import { machine, useContext, useState } from './my-state-machine';
 
 
-const vacancyMachine = machine({
-    id: 'vacancy',
-    initialState: 'notResponded',
-    context: {id: 123},
+const myMachine3 = machine({
+    initialState: 1,
+    context: { b: 'myData' },
     states: {
-        responded: {
-            onEntry: 'onStateEntry',
+        '2': {
+            onEntry: () => {
+                console.log('onEntry', useState());
+            },
+        },
+        '1': {
             on: {
-                UNRESPOND: {
-                    service: (event) => {
-                        const [contex, setContext] = useContext();			
-                        const [state, setState] = useState();
-                        return new Promise((resolve, reject) => {
-                            setTimeout(() => reject(), event);
-                            }).then(() => {
-                            setState('notResponded');
-                            setContext({completed: 'UNRESPOND'});
-                        })
-                    },
-                },
-                RESPOND: {
-                    service: (event) => {
-                        console.log('i\'v already been responded');
+                go: {
+                    service: () => {
+                        const [context, setContext] = useContext();
+                        const [_, setState] = useState();
+                        console.log(useContext()); // [ 'myData', [Function: bound setContext] ]
+                        console.log(useState()); // [ 1, [Function: bound setState] ]
+                        
+                        setTimeout(() => {
+                            setState('2');
+                            setContext({ a: 1 });
+                            console.log(context);
+                        });
                     },
                 },
             },
         },
-        notResponded: {
-            onExit: 'onStateExit',
-            on: {
-                RESPOND: {
-                    service: (event) => {
-                        const [contex, setContext] = useContext();			
-                        const [state, setState] = useState();
-                        return new Promise((resolve, reject) => {
-                            setTimeout(() => resolve(), event);
-                            }).then(() => {
-                            setState('responded');
-                            setContext({completed: 'RESPOND'});
-                        })
-                    },
-                },
-                UNRESPOND: {},
-            },
-        },		
     },
-    actions: {
-        onStateEntry: (event) => {
-            const [state] = useState();
-            console.log('now state is ' + state);
-        },
-        onStateExit: (event) => {
-            const [state] = useState();
-            console.log('now state is ' + state);
-        },
-    }
-})
+});
 
-const vacancyMachine2 = machine({
-    id: 'vacancy2',
-    initialState: 'notResponded',
-    context: {id: 456},
+const myMachine = machine({
+    initialState: 1,
+    context: { b: 'myData' },
     states: {
-        responded: {
-            onEntry: 'onStateEntry',
+        '2': {},
+        '1': {
             on: {
-                UNRESPOND: {
-                    service: (event) => {
-                        const [contex, setContext] = useContext();			
-                        const [state, setState] = useState();
-                        return new Promise((resolve, reject) => {
-                            setTimeout(() => resolve(), event);
-                            }).then(() => {
-                            setState('notResponded');
-                            setContext({completed: 'UNRESPOND'});
-                        })
+                go: {
+                    service: () => {
+                        myMachine2.transition('go');
+                        console.log(useContext()[0]); // Здесь будет выведено { b: 'Machine2 data' } и работа будет с myMachine2, хотя функция внутри myMachine
                     },
                 },
             },
         },
-        notResponded: {
-            onExit: 'onStateExit',
-            on: {
-                RESPOND: {
-                    service: (event) => {
-                        const [contex, setContext] = useContext();			
-                        const [state, setState] = useState();
-                        return new Promise((resolve, reject) => {
-                            setTimeout(() => resolve(), event);
-                            }).then(() => {
-                            setState('responded');
-                            setContext({completed: 'RESPOND'});
-                        })
-                    },
-                },
-            },
-        },		
     },
-    actions: {
-        onStateEntry: (event) => {
-            const [state] = useState();
-            console.log('now state is ' + state);
-        },
-        onStateExit: (event) => {
-            const [state] = useState();
-            console.log('now state is ' + state);
-        },
-    }
-})
+});
 
-vacancyMachine2.transition('RESPOND', 10);
-vacancyMachine2.transition('UNRESPOND', 10);
-vacancyMachine2.transition('RESPOND', 10);
-vacancyMachine2.transition('UNRESPOND', 10);
+const myMachine2 = machine({
+    initialState: 1,
+    context: { b: 'Machine2 data' },
+    states: {
+        '2': {},
+        '1': {
+            on: {
+                go: {
+                    service: () => {
+                        console.log(useContext()[0]); 
+                    },
+                },
+            },
+        },
+    },
+});
 
-setTimeout(() => vacancyMachine.transition('RESPOND', 50), 100);
-setTimeout(() => vacancyMachine.transition('UNRESPOND', 50), 160);
-// когда в transition выставлен флаг true, то будет произведена попытка
-// повторить по новой старую транзакцию, которая вызвала ошибку в сервисе
-setTimeout(() => vacancyMachine.transition('RESPOND', 50, true), 250)
-// если выставлен флаг false, то не будет производиться попытка провести 
-// старые транзакции, а всё начнется с нуля.
-setTimeout(() => vacancyMachine.transition('RESPOND', 50, false), 350)
+myMachine3.transition('go', 0);
+setTimeout(() => {
+    console.log();
+    myMachine.transition('go');
+}, 400);
